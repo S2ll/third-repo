aa484e7ef5268b320f7c027045791317e33ff33b
Rnfrld <ohseil914@naver.com>
2023-03-28 14:35:07 +0900

    커밋 2 메소드를 추가로 인한 코드 수정 빼기 기능 완성

--- calc.py
+++ calc.py
@@ -1,21 +1,24 @@
 ## 함수 선언부(=메소드)
+def add_func(n1, n2) :
+    retValue = n1 + n2
+    return retValue
 
-
-
-
-
+def sub_func(n1, n2) :
+    retValue = n1 - n2
+    return retValue
 
 
 
 ## 전역 변수부
-num1, num2, sum = 100, 200, 0
-
-
+num1, num2, result = 100, 200, 0
 
 
 
 
 
 ## 메인 코드부
-sum = num1 + num2
-print(num1, "+", num2, "=", sum)
+result = add_func(num1, num2)
+print(num1, "+", num2, "=", result)
+
+result = sub_func(num1, num2)
+print(num1, "-", num2, "=", result)
